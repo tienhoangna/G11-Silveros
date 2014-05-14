4425085979906a2d12dd0296dd0ce23ad9cc33e9
ThitGaRang <ly.phuong.anh.189@gmail.com>
2014-05-14 21:19:11 +0700

    test edit users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ SampleApp::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   root  'static_pages#home'
+  match '/edituser', to: 'users#edit', via: 'get'
   match '/home',    to: 'static_pages#home',     via: 'get'
   match '/help',    to: 'static_pages#help',     via: 'get'
   match '/about',   to: 'static_pages#about',    via: 'get'
