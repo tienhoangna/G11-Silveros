c294105600bbcf457a43c71abf44752741a8ea99
VanHuy <huydoanvan9x@gmail.com>
2014-05-16 02:08:53 +0700

    Edit utilities

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,22 +1,23 @@
 #return the full title on a per-page basis
-	def full_title(page_title)
-		base_title = "Ruby on Rails"
-		if (page_title.empty? )
-			base_title
-		else 
-			"#{base_title} | #{page_title}"
-		end
-	end
-	def sign_in(user, options={})
-  		if options[:no_capybara]
-    	# Sign in when not using Capybara.
-    		remember_token = User.new_remember_token
-    		cookies[:remember_token] = remember_token
-    		user.update_attribute(:remember_token, User.digest(remember_token))
-  		else
-    		visit signin_path
-    		fill_in "Email",    with: user.email
-    		fill_in "Password", with: user.password
-    		click_button "Sign in"
-  		end
-	end
+#return the full title on a per-page basis
+include ApplicationHelper
+
+def signin(user, options={})
+  if options[:no_capybara]
+    # Sign in when not using Capybara as well.
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.digest(remember_token))
+  else
+    visit signin_path
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text: message)
+  end
+end
