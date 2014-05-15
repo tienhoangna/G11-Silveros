dbaf7f7f572d6659070f69a23527e7a0aaf0536a
VanHuy <huydoanvan9x@gmail.com>
2014-05-16 02:23:10 +0700

    fix conflict Create profile's test and page

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,15 @@ SampleApp::Application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   root  'static_pages#home'
   
-  match '/signup',  to: 'users#new',            via: 'get'
-  match '/signin',  to: 'sessions#new',         via: 'get'
-  match '/signout', to: 'sessions#destroy',     via: 'delete'
-  match '/help',    to: 'static_pages#help',    via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/user', to: 'static_pages#profile', via: 'get'
+  match '/edit_user', to: 'users#edit', via: 'get'
+  match '/home',    to: 'static_pages#home',     via: 'get'
+  match '/help',    to: 'static_pages#help',     via: 'get'
+  match '/about',   to: 'static_pages#about',    via: 'get'
+  match '/contact', to: 'static_pages#contact',  via: 'get'
+  match '/signup',  to: 'users#new',             via: 'get'
+  match '/signin',  to: 'sessions#new',          via: 'get'
+  match '/signout', to: 'sessions#destroy',      via: 'delete'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
