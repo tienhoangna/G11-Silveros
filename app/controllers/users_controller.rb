e9a9ba95399b37d6cc878e618c8312487ee2058d
ThitGaRang <ly.phuong.anh.189@gmail.com>
2014-05-22 06:33:40 +0700

    Slide Bao Cao

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create 
   	@user = User.new(user_params)  #not final implementation
   	if @user.save
-      flash[:success] = "Welcome to the Silveros"
+      flash[:success] = "Welcome to Silveros"
   		redirect_to @user
   	else
   		render 'new'
