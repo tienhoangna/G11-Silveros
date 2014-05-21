981cfe787234a5f1ab24b31903c5af596ac67925
ThitGaRang <ly.phuong.anh.189@gmail.com>
2014-05-22 06:27:21 +0700

    Slide tong ket

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       signin(@user)
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to Silveros!"
       redirect_back_or @user
     else
       render 'new'
